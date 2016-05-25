58097700b46efd0572b64f1bac59f49b37bf3aed
Maria_Akulova <Maria_Akulova@epam.com>
2016-05-25 15:21:59 +0300

    Initial commit

---
+++ src/test/java/epam/com/AcceptanceTestSuite.java
@@ -0,0 +1,8 @@
+package epam.com;
+
+public class AcceptanceTestSuite extends OptimizedParallelAcceptanceTestSuite {
+    public AcceptanceTestSuite() {
+        selectStoryFilesForRunningSuite();
+    }
+
+}
